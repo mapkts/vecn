a2de974e5292963ee09f8cffcf19804ba8417be9
Aaron Taner <mapkts@gmail.com>
2021-04-25 22:53:44 +0800

    docs: update readme

--- crates/vecn/src/lib.rs
+++ crates/vecn/src/lib.rs
@@ -1,3 +1,17 @@
+//! [![Crates.io][crates-badge]][crates-url]
+//! [![Docs.rs][docs-badge]][docs-url]
+//! [![MIT licensed][mit-badge]][mit-url]
+//! [![Build Status][actions-badge]][actions-url]
+//! 
+//! [crates-badge]: https://img.shields.io/crates/v/vecn.svg
+//! [crates-url]: https://crates.io/crates/vecn
+//! [docs-badge]: https://docs.rs/vecn/badge.svg
+//! [docs-url]: https://docs.rs/vecn
+//! [mit-badge]: https://img.shields.io/badge/license-MIT-blue.svg
+//! [mit-url]: https://github.com/mapkts/vecn/blob/master/LICENSE
+//! [actions-badge]: https://github.com/mapkts/vecn/workflows/CI/badge.svg
+//! [actions-url]: https://github.com/mapkts/vecn/actions?query=workflow%3ACI+branch%3Amaster
+//!
 //! Provides the `#[vector]` macro, which transforms user-defined structs into general vector
 //! types by conditionally implementing appropriate traits and methods for them.
 //!
